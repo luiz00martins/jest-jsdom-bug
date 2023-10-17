7c16b7ba5dfe01f2887abe2374be832a067b6cca
luiz00martins <luizhomartins@gmail.com>
2023-10-17 00:49:29 -0300

    fix: add succeeding schema

--- __tests__/test.js
+++ __tests__/test.js
@@ -12,8 +12,12 @@ describe('Test', function() {
 		},
 	};
 
-	it('should run schema', async () => {
+	it('should fail schema', async () => {
 		expect(alterschema(schema, 'draft4', 'draft6')).toEqual(schema);
+	});
+
+	it('should succeed schema', async () => {
+		expect(await alterschema(schema, 'draft4', 'draft6')).toEqual(schema);
 	});
 });
 
